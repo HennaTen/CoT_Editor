e948499829ece133a7a05136771076745f08a303
Touitoui <61058169+HennaTen@users.noreply.github.com>
2025-02-17 18:25:26 +0100

    improved right click code, inserts now auto load

--- app/gui/elements/right_click_menu.py
+++ app/gui/elements/right_click_menu.py
@@ -2,46 +2,43 @@ import tkinter as tk
 from app.tools.json_convert import convert_dir_to_dict, convert_to_dict, convert_to_json
 
 
-
 class Submenu:
     menu_elements = {}
 
-    def __init__(self, root, selected):
+    def __init__(self, root, items=None, items_dir=None):
         self.root = root
         self.menu = tk.Menu(self.root, tearoff=0)
-        self.selected = None
-        self.items = None
-        # self.update(selected)
+        self.items = items
+
+        if items_dir:
+            try:
+                print(f"Using {items_dir}")
+                self.items = convert_to_dict(items_dir)
+            except FileNotFoundError:
+                print(f"Error loading {items_dir}")
 
 
-    def update(self, selected, ranges, content_text):
-        self.selected = selected
+class InsertsMenu(Submenu):
+    def __init__(self, root, items, func):
+        Submenu.__init__(self, root, items=items)
+        for key in self.items.keys():
+            self.menu.add_command(label=key, command=lambda x=key: func(var_=x))
 
 
 class MenuColorEffect(Submenu):
-    def __init__(self, root, selected):
-        Submenu.__init__(self, root, selected)
-
-
-    def update(self, selected, ranges, content_text, event=None):
-        self.selected = selected
-        self.menu.delete(0, tk.END)
-        # Check if config/containers/color_classes.json exists and use it, else use app.data.cot_data.color_classes
-        try:
-            self.items = convert_to_dict("config/containers/color_classes.json")
-            print("Using color_classes.json")
-        except FileNotFoundError:
+    def __init__(self, root, func, items=None, items_dir=None):
+        Submenu.__init__(self, root, items_dir=items_dir)
+        if not self.items:
             from app.data.cot_data import color_classes
             self.items = color_classes
             print("Using color_classes from cot_data")
 
+        begin = "<<highlight "
+        middle = ">>"
+        end = "<</highlight>>"
         for key in self.items.keys():
-
-            def replace(color_name):
-                content_text.replace(*ranges, f"<<highlight {color_name}>>{self.selected}<</highlight>>")
-
             color = self.items[key]
-            self.menu.add_command(label=key, background=color, command=lambda x=key: replace(x))
+            self.menu.add_command(label=key, background=color, command=lambda x=key: func(x, begin, middle, end))
 
 
 class SexyRightClickMenu:
@@ -49,11 +46,17 @@ class SexyRightClickMenu:
         self.root = root
         self.content_text = content_text
         self.menu = None
-        self.selection = None
         self.ranges = None
-        self.submenu = {
-            "color": MenuColorEffect(self.root, self.selection)
+        self.replace_submenu = {
+            "color": MenuColorEffect(self.root, items_dir="config/containers/highlight.json", func=self.replace),
         }
+
+        self.insert_submenus = {}
+        inserts = convert_dir_to_dict("config/inserts")
+        for item_name in inserts.keys():
+            items = inserts[item_name]
+            self.insert_submenus[item_name] = InsertsMenu(self.root, items=items, func=self.insert)
+
 
     def click(self, event=None):
         print("right click")
@@ -63,51 +66,29 @@ class SexyRightClickMenu:
         self.ranges = self.content_text.tag_ranges(tk.SEL)
 
         if self.ranges:
-            self.selection = self.content_text.get(*self.ranges)
-            print(f'SELECTED Text is {self.selection} in range {self.ranges}')
             self.menu_selection()
         else:
-            print('NO Selected Text')
             self.menu_insert()
         try:
             self.menu.tk_popup(event.x_root, event.y_root)
         finally:
             self.menu.grab_release()
             self.menu = None
-            self.selection = None
 
     def menu_selection(self, event=None):
         print("menu selection")
-        self.update_submenus()
-
-        self.menu.add_cascade(label="<<highlight>>", menu=self.submenu["color"].menu)
-
-        self.menu.add_command(label=f"It will have other options to apply tags to {self.selection} here")
-        # if self.selection in pronoun_tags.keys():
-        #     self.menu.add_command(label=f"{self.selection}", background="lightblue")
-        #     self.menu.add_command(label=f"{pronoun_tags[self.selection]}")
-        # elif self.selection in pronoun_tags.keys():
-        #     self.menu.add_command(label=f"{pronoun_tags[self.selection]}")
-        #     self.menu.add_command(label=f"{self.selection}", background="lightblue")
-        # self.menu.add_separator()
-        # self.menu.add_command(label="Replace selection menu here")
-        #
-        # self.menu.add_separator()
-        # self.menu.add_command(label="Info")
+        for key in self.replace_submenu.keys():
+            self.menu.add_cascade(label=key, menu=self.replace_submenu[key].menu)
 
     def menu_insert(self, event=None):
         print("menu insert")
-        self.menu.add_command(label="It will have options to insert elements here")  # , command=lambda: self.test_insert(var_="Test"))
-        #self.menu.add_separator()
-        # self.menu.add_command(label="Insert menu here")
-        # self.menu.add_command(label="Like Link/skillgates")
+        for key in self.insert_submenus.keys():
+            self.menu.add_cascade(label=key, menu=self.insert_submenus[key].menu)
 
-    def update_submenus(self):
-        for key in self.submenu.keys():
-            self.submenu[key].update(self.selection, self.ranges, self.content_text)
-
-    def test_insert(self, event_=None, var_=None):
+    def insert(self, var_=None):
         self.content_text.insert(tk.INSERT, var_)
 
-    def test_replace(self, event_=None, var_=None):
-        self.content_text.insert(tk.INSERT, var_)
+    def replace(self, var_, begin="<<", middle= "", end=">>"):
+        ranges = self.content_text.tag_ranges(tk.SEL)
+        selection = self.content_text.get(*self.ranges)
+        self.content_text.replace(*ranges, f"{begin}{var_}{middle}{selection}{end}")
